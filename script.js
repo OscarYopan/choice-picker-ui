6e5e9a58c3e7efcb9fe12484b54469b1ca61e32c
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-07-21 11:12:58 -0300

    --

--- script.js
+++ script.js
@@ -5,15 +5,37 @@ textarea.focus() //Quando carrega a pagina o cursor vai direto
 
 textarea.addEventListener('keyup', (element) => {
   createTags(element.target.value)
+  if (element.key === 'Enter') {
+    setTimeout(() => { // funcao para limpar
+      element.target.value = '' 
+    }, 10) //tempo de espera em 'ms'
+
+    randomSelect()
+  }
 })
 
 function createTags (input) {
-  const tags = input.split(',') // split para separar os valores do 'arrow'
+  const tags = input.split(',') // '.split' para separar os valores do 'arrow'
   
   tagsEl.innerHTML = ''
+
   tags.forEach(tag => {
     const tagEl = document.createElement('span')
     tagEl.classList.add('tag')
-    
+    tagEl.innerText = tag
+    tagsEl.appendChild(tagEl)
   });
+}
+
+ function randomSelect () {
+  const times = 30
+  
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+  }, 100) // intervalo em 'ms'
+}
+
+function pickRandomTag () {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
 }
